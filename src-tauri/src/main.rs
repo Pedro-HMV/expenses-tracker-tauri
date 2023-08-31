ea6ab571dde216a46e362b8dae0271771a7f6a7f
Pedro Vilela <pedroh_mvilela@hotmail>
2023-08-31 14:53:09 -0300

    add leap year check to check_valid_day function

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -80,10 +80,12 @@ fn sum_expenses(data: &Content) -> f32 {
 
 fn check_valid_day(day: u32) -> bool {
     let month = Local::now().month();
+    let leap_year = Local::now().year() % 4 == 0;
     let valid = match month {
         1 | 3 | 5 | 7 | 8 | 10 | 12 => day <= 31,
         4 | 6 | 9 | 11 => day <= 30,
-        2 => day <= 29,
+        2 if leap_year => day <= 29,
+        2 => day <= 28,
         _ => false,
     };
     valid
